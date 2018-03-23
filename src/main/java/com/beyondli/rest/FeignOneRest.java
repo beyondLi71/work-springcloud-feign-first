eb5542dc2eabb85fd5d6039626da98c79be938f2
liboyang <liboyang@maxrocky.com>
2018-03-23 16:08:34 +0800

    新增config连接配置

--- src/main/java/com/beyondli/rest/FeignOneRest.java
+++ src/main/java/com/beyondli/rest/FeignOneRest.java
@@ -1,6 +1,8 @@
 package com.beyondli.rest;
 
 import com.beyondli.service.FeignOneService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +14,14 @@ import javax.annotation.Resource;
  * Date 2018/3/15
  * Desc .S
  */
+@RefreshScope
 @RestController
 @RequestMapping(value = "/one")
 public class FeignOneRest {
     @Resource
     FeignOneService feignOneService;
+    @Value("${title}")
+    private String title;
     /**
      * 测试一号是否
      * @return
@@ -33,6 +38,15 @@ public class FeignOneRest {
     public String getTwo() {
         String info = feignOneService.getTwo();
         return info;
+    }
+
+    /**
+     *获取配置文件的信息
+     */
+    @RequestMapping(value = "/get/config", method = RequestMethod.GET)
+    public String getConfig() {
+        //return "ok";
+        return title;
     }
 
 }
